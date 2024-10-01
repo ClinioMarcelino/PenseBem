a10129d5c7551b3fd9a28217b29c51c76e37d06f
ClinioMarcelino <cliniof@gmail.com>
2024-10-01 17:54:07 -0300

    new funcionalities

--- public/sricpt.js
+++ public/sricpt.js
@@ -1,7 +1,11 @@
 let currentPage = 1;
 let pdfDoc = null;
+let gabarito = "A";
+let pergunta = 0;
+
 const canvas = document.getElementById('pdf-canvas');
 const ctx = canvas.getContext('2d');
+const answerButtons = document.querySelectorAll('.button');
 
 // Carregar o PDF do servidor
 pdfjsLib.getDocument('/formula1.pdf').promise.then(function (pdf) {
@@ -29,9 +33,51 @@ function renderPage(num) {
 }
 
 // Adiciona a funcionalidade de clicar no quadrado vermelho para avançar as páginas
-document.querySelector('.red-square').addEventListener('click', function () {
+function troca_pagina() {
+    // document.getElementsByClassName()
     if (currentPage < pdfDoc.numPages) {
         currentPage++;
         renderPage(currentPage);
     }
+};
+
+answerButtons.forEach(button => {
+    button.addEventListener('click', function() {
+      const userAnswer = this.getAttribute('data-'); // Obter a resposta selecionada
+      const Element = document.querySelector('.screen-text'); // Elemento onde exibiremos o resultado
+    
+      // Verificar se a resposta está correta
+        if (userAnswer === gabarito) {
+            // const Element = document.querySelector('.screen-text');
+            Element.textContent = 'Certo';
+            Element.style.color = 'green';
+
+            // TODO!
+            if(true){
+                troca_pagina();
+            }
+        }
+        else{
+            Element.textContent = 'Errado';
+            Element.style.color = 'red'; 
+        }
+    })
 });
+
+
+
+// function altera_janela(resposta){
+//     document.getElementsByClassName("screen-text").textContent = resposta;
+// }
+
+function escrever_tela(str) {
+    const Element = document.querySelector('.screen-text');
+    if (str === 1) {
+      Element.textContent = 'Certo';
+      Element.style.color = 'green';  
+    }
+    else{
+        Element.textContent = 'Errado';
+      Element.style.color = 'red'; 
+    }
+  }
